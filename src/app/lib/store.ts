53db48bd118eb835d027dc3e1063500209cd5b54
Ge0rg3e <georgedeliu2006@gmail.com>
2026-02-04 09:59:54 +0200

    feat(store): add volume and mute state with persistence

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -5,27 +5,26 @@ interface AppState {
 	isSidebarOpen: boolean;
 	toggleSidebar: () => void;
 	closeSidebar: () => void;
+	volume: number;
+	setVolume: (volume: number) => void;
+	isMuted: boolean;
+	setIsMuted: (isMuted: boolean) => void;
 }
 
 export const useAppStore = create<AppState>()(
 	persist(
 		(set, get) => ({
 			isSidebarOpen: true,
-			toggleSidebar: () => {
-				set((state) => {
-					const newState = { isSidebarOpen: !state.isSidebarOpen };
-					console.log('toggleSidebar - new state:', newState.isSidebarOpen);
-					return newState;
-				});
-			},
-			closeSidebar: () => {
-				set({ isSidebarOpen: false });
-				console.log('closeSidebar - new state: false');
-			}
+			volume: 100,
+			setVolume: (volume: number) => set({ volume }),
+			isMuted: false,
+			setIsMuted: (isMuted: boolean) => set({ isMuted }),
+			toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+			closeSidebar: () => set({ isSidebarOpen: false })
 		}),
 		{
 			name: '@app',
-			partialize: (state) => ({ isSidebarOpen: state.isSidebarOpen })
+			partialize: (state) => ({ isSidebarOpen: state.isSidebarOpen, volume: state.volume, isMuted: state.isMuted })
 		}
 	)
 );
